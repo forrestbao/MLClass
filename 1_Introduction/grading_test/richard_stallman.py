544de7df57fe0ddb9affa14f676b1083bfd34c97
forrest <forrest.bao@gmail.com>
2020-09-09 23:58:37 -0500

    update grading script

---
+++ 1_Introduction/grading_test/richard_stallman.py
@@ -0,0 +1,6 @@
+def f(a, b, c):
+    """
+    a, b: 1-D numpy.ndarray
+    c: str, place holder
+    """
+    return a+b, -10000, a*b
--- make.sh
+++ make.sh
@@ -1,1 +1,3 @@
-pandoc -t beamer 1_intro.md -o 1_intro.pdf #  -i 
+#pandoc -t beamer 1_intro.md -o 1_intro.pdf #  -i 
+#pandoc -t beamer 2_linear_classifiers.md -o 2_linear_classifiers.pdf   -i 
+pandoc -t beamer 3_decision_trees.md -o 3_decision_trees.pdf   -i 
