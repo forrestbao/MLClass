23e906ab6577f7033083084fb8b4bca1866b3a1a
forrest <forrest.bao@gmail.com>
2020-09-13 23:37:27 -0500

    add homework 2

---
+++ 2_Linear_Classifiers/hw2.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+def plot_mse(X, y, filename):
+    w = np.array([0,0,0]) # just a placeholder
+
+    # your code here
+
+
+    return w
+
+def plot_fisher(X, y, filename): 
+    w = np.array([0,0,0]) # just a placeholder
+    # your code here 
+
+    return w
